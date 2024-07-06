412235c972415133126e54e63b0670da035a200f
Gonzalo Ariel Spesot <gonzalospesot7@gmail.com>
2024-07-06 14:33:23 -0300

    "1"

--- proyecto/app.py
+++ proyecto/app.py
@@ -1,19 +1,20 @@
-from flask import Flask ,jsonify ,request
-# del modulo flask importar la clase Flask y los métodos jsonify,request
+from flask import Flask ,jsonify ,request # del modulo flask importar la clase Flask y los métodos jsonify,request
 from flask_cors import CORS       # del modulo flask_cors importar CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-app=Flask(__name__)  # crear el objeto app de la clase Flask
-CORS(app) #modulo cors es para que me permita acceder desde el frontend al backend
-# configuro la base de datos, con el nombre el usuario y la clave
+
+app=Flask(__name__)  
+CORS(app) 
+
 app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost/proyecto'
-# URI de la BBDD                          driver de la BD  user:clave@URLBBDD/nombreBBDD
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #none
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False 
+
+
 db= SQLAlchemy(app)   #crea el objeto db de la clase SQLAlquemy
 ma=Marshmallow(app)   #crea el objeto ma de de la clase Marshmallow
 
 
-#—---
+
 
 # defino las tablas
 class Producto(db.Model):   # la clase Producto hereda de db.Model de SQLAlquemy   
